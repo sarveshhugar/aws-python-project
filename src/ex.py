1fbdb92ad8ea69f4fa583679fe4dadb658167411
sarveshhugar <sarvesh.hugar@7edge.com>
2023-02-16 17:10:23 +0530

    revert back to original

---
+++ src/ex.py
@@ -0,0 +1,37 @@
+# import uuid
+# print(type(str(uuid.uuid1())))
+# d={
+#     "a":10,
+#     "b":11
+# }
+# k={"c":12
+#      }
+# # l= {'email': data['email'],
+# #         'name':data['name'] ,
+# #         'YOE':data['YOE'],
+# #         'company':data['company']}
+# k.update(d)
+# print(k)
+# l=[  ]
+k=''
+data={"name":"abc",
+      "age":"23",
+      "email":"ahgdha",
+      "company":"hbac"}  
+# for col in data:
+#     if col!="company" and col!="email":
+#         k='#{}'.format(col)
+#         print(k)
+#         k='SET #{} = {}'.format(col,data[col])
+#         print(k)
+# update_expression= 'SET {}'.format(','.join(f'#{p}=:{p}' for p in data if p!="email" and p!="company"))
+# update_expression= 'SET {}'.format(','.join(f'#{p}=:{p}' for p in data if p!="email" and p!="company"))
+# expression_attribute_values= {f':{p}': v for p,v in data.items() if p!="email" and p!="company"}
+# expression_attribute_names= { f'#{p}': p for p in data if p!="email" and p!="company"}
+# import time
+# print(update_expression)
+# print(expression_attribute_names)
+# print(expression_attribute_values)
+
+    
+# print(str(time.ctime(time.time())))
