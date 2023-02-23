1309ad75becd4843d6b98572fb30e982d0520571
sarveshhugar <sarvesh.hugar@7edge.com>
2023-02-23 15:13:54 +0530

    added orderby by DESC/ASC

--- src/search.py
+++ src/search.py
@@ -10,18 +10,20 @@ table=dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 def search(event,context):
     data=json.loads(event["body"])
     
+    #searching using GSI
     if "IndexName" in data:
-        if "IndexName" not in data and "companytype" not in data:
+        if "companytype" not in data:
              logging.error("Search failed")
              raise Exception("Couldn't search for item.")
-        Filter_expression= '{}'.format(' AND '.join(f'#{p}=:{p}' for p in data if p!="IndexName" and p!="companytype"))
-        Expression_Attribute_Names={ f'#{p}': p for p in data if p!="IndexName" and p!="companytype" }
+        Filter_expression= '{}'.format(' AND '.join(f'#{p}=:{p}' for p in data if p!="IndexName" and p!="companytype" and p!="sortby" and p!="orderby"))
+        Expression_Attribute_Names={ f'#{p}': p for p in data if p!="IndexName" and p!="companytype" and p!="sortby" and p!="orderby"}
 
         Expression_attribute_values1={":companytype": data["companytype"]}
-        Expression_attribute_values2= { f':{p}': data[p] for p in data if p!="IndexName" and p!="companytype"}
+        Expression_attribute_values2= { f':{p}': data[p] for p in data if p!="IndexName" and p!="companytype" and p!="sortby" and p!="orderby"}
 
         Expression_attribute_values1.update(Expression_attribute_values2)
 
+        #if there are non-key attributes also 
         if len(Filter_expression)!=0 and len(Expression_Attribute_Names)!=0:
                 response=table.query(
                     IndexName=data["IndexName"],
@@ -31,6 +33,7 @@ def search(event,context):
                     ExpressionAttributeNames = Expression_Attribute_Names
 
                     )
+        #if there is only gsi index
         else:
                 response=table.query(
                 IndexName=data["IndexName"],
@@ -38,23 +41,25 @@ def search(event,context):
                 ExpressionAttributeValues = Expression_attribute_values1
                 )
 
+    #searching using Primary Key
     else:
-        Filter_expression= '{}'.format(' AND '.join(f'#{p}=:{p}' for p in data if p!="company" and p!="email" and p!="sortby"))
+        Filter_expression= '{}'.format(' AND '.join(f'#{p}=:{p}' for p in data if p!="company" and p!="email" and p!="sortby" and p!="orderby"))
         
+        #using  sortkey
         if "email" in data:
              Key_Condition_Expression="#company= :company AND #email =:email"
              Expression_attribute_values={":company":data["company"],
-                                          ":email":data["email"]}
-            
+                                          ":email":data["email"]}    
         else:
              Key_Condition_Expression="#company= :company"
              Expression_attribute_values={":company":data["company"]}
             
         
-        Expression_attribute_values2= { f':{p}': data[p] for p in data if p!="company" and p!="sortby"}
-        Expression_Attribute_Names={ f'#{p}': p for p in data if p!="sortby"}
+        Expression_attribute_values2= { f':{p}': data[p] for p in data if p!="company" and p!="sortby" and p!="orderby" }
+        Expression_Attribute_Names={ f'#{p}': p for p in data if p!="sortby" and p!="orderby"}
         Expression_attribute_values.update(Expression_attribute_values2)
 
+        #if there are non-key attributes also
         if len(Filter_expression)!=0 and len(Expression_Attribute_Names)!=0:
             response=table.query(
                 KeyConditionExpression = Key_Condition_Expression,
@@ -62,6 +67,7 @@ def search(event,context):
                 ExpressionAttributeNames = Expression_Attribute_Names,
                 FilterExpression = Filter_expression
             )
+        #if there are only key attributes
         else:
             response=table.query(
                 KeyConditionExpression = Key_Condition_Expression,
@@ -69,28 +75,42 @@ def search(event,context):
                 ExpressionAttributeNames = Expression_Attribute_Names
             )
     
-    
+    #if we receive matching items from query
     if len(response["Items"])!=0:   
-
         if "sortby" in data:
              l,Items=[],response["Items"]
-             try:
-                for item in sorted(Items,key = lambda item : item[data["sortby"]]):
-                    l.append(item)
-             except:
-                   logging.error("Invalid attribute")
+             if not "orderby" in data:
+                try:
+                    for item in sorted(Items,key = lambda item : item[data["sortby"]]):
+                        l.append(item)
+                except:
+                    logging.error("Invalid attribute")
 
+                else:
+                    return {
+                    "statusCode":200,
+                    "body":json.dumps(l)
+                    }
              else:
-                return {
-                "statusCode":200,
-                "body":json.dumps(l)
-                }
+                try:
+                    for item in sorted(Items,key = lambda item : item[data["sortby"]],reverse=True if data["orderby"]=="DESC" else False) :
+                        l.append(item)
+                except:
+                    logging.error("Invalid attribute")
+
+                else:
+                    return {
+                    "statusCode":200,
+                    "body":json.dumps(l)
+                    }
+
         else:
             return {
              "statusCode":200,
              "body":json.dumps(response["Items"])
             }
     
+    #if there are no items matched
     else:
          return {
               "statusCode":200,
