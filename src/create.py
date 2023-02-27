87dbb007b6b63fd974f9fd6be5abe5174cbf6890
sarveshhugar <sarvesh.hugar@7edge.com>
2023-02-27 12:27:54 +0530

    Check for the data if it exists before creating in dynamodb

--- src/create.py
+++ src/create.py
@@ -10,27 +10,46 @@ dynamodb = boto3.resource('dynamodb')
 
 def create(event, context):
     data = json.loads(event['body'])
-    if 'email' not in data["item"]:
+    if 'email' not in data and "company" not in data:
         logging.error("Validation Failed")
         raise Exception("Couldn't create the item.")
 
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
     
-    timestamp=str(time.ctime())
-    item1 = {
-        'empid':str(uuid.uuid1()),
-        'createdOn':timestamp
-    }
-    item2=data["item"]
+    #to check if the data already exist
+    checker =table.query(
+        KeyConditionExpression=" #company= :company AND #email= :email",
+        ExpressionAttributeValues={":company":data["company"],
+        ":email":data["email"]},
+        ExpressionAttributeNames={ "#company":"company","#email":"email" }
+    )
+    if len(checker["Items"])>0:
+        return {
+            "statusCode":400,
+            "body":json.dumps({
+                "statusCode":400,
+                "message": "The user already exist"
+            })
+        }
+    else:
+    
+        timestamp=str(time.time())
+        item1 = {
+            'empid':str(uuid.uuid1()),
+            'createdOn':timestamp
+        }
 
-    item1.update(item2)
-    # write the todo to the database
-    table.put_item(Item=item1)
+        item2={"{}".format(item):data[item] for item in data }
+        item1.update(item2)
+        # write the todo to the database
+        table.put_item(Item=item1)
 
-    # create a response
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(item1)
-    }
+        # create a response
+        response = {
+            "statusCode": 200,
+            "body": json.dumps({
+                "statusCode":200,
+                "ItemCreated":item1})
+        }
 
-    return response
+        return response
