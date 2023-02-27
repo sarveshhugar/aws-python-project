e0ebf4bcf0f2385bd122c6c0d04cd4a90c23ac08
sarveshhugar <sarvesh.hugar@7edge.com>
2023-02-27 12:30:54 +0530

    added try except blocks

--- src/update.py
+++ src/update.py
@@ -3,7 +3,7 @@ import os
 import boto3
 import logging
 dynamodb = boto3.resource('dynamodb')
-# table = dynamodb.Table('7EDGE-EMPLOYEE')
+
 
 def update(event, context):
     data = json.loads(event['body'])
@@ -11,31 +11,40 @@ def update(event, context):
         logging.error("Validation Failed")
         raise Exception("Couldn't update the item.")
     
-
+    try:
     # update the employee detail in the database
-    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-    update_expression= 'SET {}'.format(','.join(f'#{p}=:{p}' for p in data if p!="email" and p!="company"))
-    expression_attribute_values= {f':{p}': v for p,v in data.items() if p!="email" and p!="company"}
-    expression_attribute_names= { f'#{p}': p for p in data if p!="email" and p!="company"}
+        table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+        update_expression= 'SET {}'.format(','.join(f'#{p}=:{p}' for p in data if p!="email" and p!="company"))
+        expression_attribute_values= {f':{p}': v for p,v in data.items() if p!="email" and p!="company"}
+        expression_attribute_names= { f'#{p}': p for p in data if p!="email" and p!="company"}
 
 
-    response= table.update_item(
-                Key={
-                    'company': event['pathParameters']['company'],
-                    'email':data['email']
-                    },
-                ExpressionAttributeNames = expression_attribute_names,
-                ExpressionAttributeValues = expression_attribute_values,
-                UpdateExpression = update_expression,
-                
-                ReturnValues='UPDATED_NEW'
-        
-         )
+        response= table.update_item(
+                    Key={
+                        'company': event['pathParameters']['company'],
+                        'email':data['email']
+                        },
+                    ExpressionAttributeNames = expression_attribute_names,
+                    ExpressionAttributeValues = expression_attribute_values,
+                    UpdateExpression = update_expression,
+                    
+                    ReturnValues='UPDATED_NEW'
+            
+            )
+    except:
+        return {
+             "statusCode": 400,
+             "body": json.dumps({ 
+                "statusCode": 400,
+                "message":"bad request"
+             })
+        }
+    else:
     # print(result)
     # create a response
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(response["Attributes"])
-    }
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(response["Attributes"])
+        }
 
-    return response
+        return response
